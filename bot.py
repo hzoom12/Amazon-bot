77415618571d2f670ebf142cb69f1fe607bae034
hzoom12 <ha12em@hotmail.com>
2026-09-25 19:50:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -109,7 +109,7 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg += f"{original_url}\n\n"
         
         # إضافة سطر الترويج
-        msg += f"{emoji_star}\nخصم اضافي مع بنك الاهلي "
+        msg += 
 
         # إرسال الرد في الخاص
         if img:
